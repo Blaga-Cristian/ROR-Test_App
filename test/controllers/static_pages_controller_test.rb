06d2c951b14687038c179fbdb3efd3f522929537
Blaga Cristian Adrian <cristiblaga2004@gmail.com>
2025-07-04 16:47:00 +0300

    Make static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+ 
+  def setup
+    @base_title = "ROR Test App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select 'title',  text: @base_title
+  end
+
+  test "should get help" do
+    get help_url
+    assert_response :success
+    assert_select 'title',  text: "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+    assert_select 'title',  text: "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_select 'title',  text: "Contact | #{@base_title}"
+  end
+
+
+end
