c2473599a0479d4a31b28ed0953efb8acc9c8b3c
Blaga Cristian Adrian <cristiblaga2004@gmail.com>
2025-07-15 13:43:09 +0300

    Change a bit of production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.public_file_server.enabled = true
-  config.assets.compile = true  # Allows fallback compilation
-  config.assets.digest = true
+  #config.public_file_server.enabled = true
+  #config.assets.compile = true  # Allows fallback compilation
+  #config.assets.digest = true
 end
