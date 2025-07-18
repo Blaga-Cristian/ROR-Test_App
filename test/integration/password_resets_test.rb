e48e57d7d99984831864a9198aa19671dcf71e0b
Blaga Cristian Adrian <cristiblaga2004@gmail.com>
2025-07-18 16:54:17 +0300

    Better test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -23,7 +23,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
       assert_not_equal @user.reset_digest, @user.reload.reset_digest
       assert_equal 1, ActionMailer::Base.deliveries.size
       assert_not flash.empty?
-      assert_redirected_to root_url
+      assert_redirected_to root_url(format: :html)
 
       #Password reset form
       user = assigns(:user)
